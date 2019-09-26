96fc8d56a1d1ed06280d5cf7e971956a75bf0d30
MuhaddiMu <Muhaddisshah@gmail.com>
2019-09-26 23:31:38 +0500

    API KEY & Call Added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from "react"
-import { BrowserRouter, Route, Switch } from "react-router-dom"
 import "./App.css"
-import Home from "./Components/Home"
-import About from "./Components/About"
-import Contact from "./Components/Contact"
-import Error from "./Components/Error"
-import Navigation from "./Components/Navigation"
+import Form from "./components/Form"
+import Recipes from "./components/Recipes"
+import { BrowserRouter, Route, Switch } from "react-router-dom"
 
-function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Navigation />
-        <Switch>
-          <Route to="/" exact component={Home}></Route>
-          <Route path="/About" component={About}></Route>
-          <Route path="/Contact" component={Contact}></Route>
-          <Route component={Error}></Route>
-        </Switch>
-      </BrowserRouter>
-    </div>
-  )
+const API_KEY = "d60a27adb9f4294b0d3044ec07285a5e"
+
+class App extends React.Component {
+  state = {
+    Recipes: []
+  }
+
+  getRecipe = async Event => {
+    Event.preventDefault()
+
+    const RecipeName = Event.target.elements.RecipeName.value
+
+    const API_CALL = await fetch(
+      `https://www.food2fork.com/api/search?key=${API_KEY}&q=${RecipeName}`
+    )
+
+    const DATA = await API_CALL.json()
+
+    this.setState({ Recipes: DATA.recipes })
+    console.log(this.state.Recipes)
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">Recipe Search</h1>
+          </header>
+          <Form getRecipe={this.getRecipe} />
+          <Recipes Recipe={this.state.Recipes} />
+        </div>
+      </React.Fragment>
+    )
+  }
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,69 +1,11 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import "./index.css"
 import App from "./App"
+import "bootstrap/dist/css/bootstrap.min.css"
+
 import * as serviceWorker from "./serviceWorker"
-import { createStore, combineReducers } from "redux"
-import { Provider } from "react-redux"
 
-//Action
-
-export const Increment = () => {
-  return {
-    type: "INCREMENT"
-  }
-}
-export const Decrement = () => {
-  return {
-    type: "DECREMENT"
-  }
-}
-
-export const LoggedIn = () => {
-  return {
-    type: "LOGGEDIN"
-  }
-}
-
-//Reducer
-const Counter = (state = 0, action) => {
-  switch (action.type) {
-    case "INCREMENT":
-      return state + 1
-    case "DECREMENT":
-      return state - 1
-    default:
-      return state
-  }
-}
-
-const Status = (state = false, action) => {
-  switch (action.type) {
-    case "LOGGEDIN":
-      return !state
-    default:
-      return state
-  }
-}
-
-//Combine Reducers
-const AllReducers = combineReducers({
-  Counter,
-  Status
-})
-
-//Store
-let store = createStore(AllReducers)
-
-//Dispatch
-store.subscribe(() => console.log(store.getState()))
-
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
-)
+ReactDOM.render(<App />, document.getElementById("root"))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

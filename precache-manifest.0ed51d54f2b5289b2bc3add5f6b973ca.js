f65727fe042fcfa5633db1e5266839d5071b8848
MuhaddiMu <Muhaddisshah@gmail.com>
2019-09-27 01:31:43 +0500

    Deploying Branch Create 🚀

---
+++ precache-manifest.0ed51d54f2b5289b2bc3add5f6b973ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd1d2de88d0034c85eb6929bc441e9c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6bc80614c4b4c4a076e9",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "dffb96819648623d75ab",
+    "url": "/static/css/main.c7c08d4e.chunk.css"
+  },
+  {
+    "revision": "6bc80614c4b4c4a076e9",
+    "url": "/static/js/2.1c13c2e8.chunk.js"
+  },
+  {
+    "revision": "dffb96819648623d75ab",
+    "url": "/static/js/main.6bda9ac3.chunk.js"
+  },
+  {
+    "revision": "9db38d1ac187fad55d6b",
+    "url": "/static/js/runtime-main.2054ec2f.js"
+  }
+]);
